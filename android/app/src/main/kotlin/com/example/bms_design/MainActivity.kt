7a0921da00edef51d8b1c74f1ec45a5392c2fde1
Parabeac-Cloud <admin@parabeac.com>
2023-07-27 11:43:22 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ android/app/src/main/kotlin/com/example/bms_design/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bms_design
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme/bms_design_text_styles.g.dart
@@ -0,0 +1,50 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class BmsDesignTextStyles {
+
+static const TextStyle subtitleSubtitle1Bold = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0.15000000596046448,
+  fontFamily: 'Inter',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle subtitleSubtitle2Bold = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0.10000000149011612,
+  fontFamily: 'Inter',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle bodyBody1 = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.5,
+  fontFamily: 'Inter',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle captionRegular = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0.4000000059604645,
+  fontFamily: 'Inter',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
